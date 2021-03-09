ac1f98c96ed7be260ecd634344ef7875f07bc92c
ralfhbryanperez <ralfhbryanperez@yahoo.com>
2021-03-09 21:38:17 +0800

    mailbox test

--- public/js/main.js
+++ public/js/main.js
@@ -252,8 +252,8 @@ $(document).ready(function() {
         }
     });
     $("#btn-add-postbox").click(function(){
-        updateCustomerData({"isNorges" : 1});
         updateCustomerData({"pb-price": 0, "mailbox-sign" : 1});
+        // updateCustomerData({"isNorges" : 1, "pb-price": 0, "mailbox-sign" : 1});
         
         sendSMS(2);
     });
