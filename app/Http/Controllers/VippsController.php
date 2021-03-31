ef826a33dc54946eab8a718133e95833144fc083
ralfhbryanperez <ralfhbryanperez@yahoo.com>
2021-03-31 19:40:38 +0800

    vipps status fixing

--- app/Http/Controllers/VippsController.php
+++ app/Http/Controllers/VippsController.php
@@ -19,27 +19,29 @@ class VippsController extends Controller
 
 	public function checker(){	
 		if(session('customer._storageToken') != ""){
-			$storage = Helper::storageStatus(Helper::getToken(), session('customer._storageToken'), "payment");
-			$arr 	 = json_decode($storage, false);
-			$status  = ""; 
+			for ($i = 0; $i <= 10 ; $i++) { 
+				$storage = Helper::storageStatus(Helper::getToken(), session('customer._storageToken'), "payment");
+				$arr 	 = json_decode($storage, false);
+				$status  = ""; 
 
-			Log::info("Checking vipps storage: ".$storage);
-			foreach ($arr->_storage_status_list as $key => $value) {
-				if($value->status == "reserved" && $value->id == "no.vipps"){
-					$status = 200;
-					$message = "Payment Success!";
-					break;
+				Log::info("Checking vipps storage: ".$storage);
+				foreach ($arr->_storage_status_list as $key => $value) {
+					if(($value->status == "reserved" || $value->status == "captured") && $value->id == "no.vipps"){
+						$status = 200;
+						$message = "Payment Success!";
+						Log::info("Vipps payment success; redirect to thank you page.");
+			    		return redirect('/takk');
+						break;
+					}
 				}
+				if($status == 200)
+					break;
 			}
 
 			if($status != 200){
 				Log::error("Vipps payment cancelled by user: ");
 				session()->put("customer.vipps-result", ["error" => "Din betaling var avvist eller avbrutt. Venligst prøv igjen."]);
-				
 	    		return redirect('/betaling/'.session('customer')['phone']);
-			}else{
-				Log::info("Vipps payment success; redirect to thank you page.");
-	    		return redirect('/takk');
 			}
 		}else{
 			Log::error("Illegal access to vipps page");
