8b5a279cf3744fb873ffa68ad2c46856917bdbf4
ralfhbryanperez <ralfhbryanperez@yahoo.com>
2021-03-31 18:08:19 +0800

    profile page fix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -41,19 +41,13 @@ class ProfileController extends Controller
         $arr = json_decode($login);
         
         if($arr->status == 10){
-    		// return redirect('/logginn')->with('error', 'Passordet er feil!')->withInput();
+    		return redirect('/logginn')->with('error', 'Passordet er feil!')->withInput();
         }else{
-        	if(session('customer')){
-	            session()->put('customer.isLogged', true);  
-	        	return redirect("/profile");
-        	}else{
-	            session()->put('customer.isLogged', true);                
-	        	return redirect("/profile");
-        		echo $arr['_token'];
-        	}
-	        //     session()->put('customer.isLogged', true);                
-
-	        // 	return redirect("/profile");
+    		$storage = Helper::getStorage(Helper::getToken(), $arr->token);
+   		 	session()->put("customer", json_decode($storage, true));
+            session()->put('customer.isLogged', true);      
+                      
+        	return redirect("/profile");
         }
         // if($login == session("customer")['number'] && $password == session("customer")['_keyLogin']){
         // 	Log::info("Customer Login: ".session('customer')['full-name']."/".date("Y-m-d H:i:s"));
