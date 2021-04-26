175ab3d0ae85d97db4f9e25bbf82c174ddcbdb15
ralfhbryanperez <ralfhbryanperez@yahoo.com>
2021-04-26 17:22:39 +0800

    working with imap

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -6,28 +6,58 @@ use Illuminate\Http\Request;
 use Helper;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
+use Webklex\IMAP\Facades\Client;
 
 class APIController extends Controller
 {
 
     public function testAPI(){
 
+        $oClient = new Client([
+            'host'          => 'smtp.mandrillapp.com',
+            'port'          => 587,
+            'encryption'    => 'ssl',
+            'validate_cert' => false,
+            'username'      => 'Nettflytt',
+            'password'      => '_4uUqOZfxOM6gbD1LL5W_w',
+            'protocol'      => 'imap'
+        ]);
+        $oClient = Client::account('default');
+        //Connect to the IMAP Server
+        $oClient->connect();
+        //Get all Mailboxes
+        /** @var \Webklex\IMAP\Support\FolderCollection $aFolder */
+        $aFolder = $oClient->getFolders();
+        //Loop through every Mailbox
+        /** @var \Webklex\IMAP\Folder $oFolder */
+        foreach($aFolder as $oFolder){
+            //Get all Messages of the current Mailbox $oFolder
+            /** @var \Webklex\IMAP\Support\MessageCollection $aMessage */
+            $aMessage = $oFolder->messages()->all()->get();
+            
+            /** @var \Webklex\IMAP\Message $oMessage */
+            foreach($aMessage as $oMessage){
+                echo $oMessage->getSubject().'<br />';
+                echo 'Attachments: '.$oMessage->getAttachments()->count().'<br />';
+                echo $oMessage->getHTMLBody(true);
+            }
+        }
            // echo $list =  Helper::storageStatus(Helper::getToken(), "mWyG79kTUYOL60u2kJZ5MlxMcore7SzmE3VJJH1bzU2xuxAVbelOCepWQjf06wBe", "info");
 
-        $file     = fopen(storage_path("app/public/tokens.txt"), "r");
-        $isHere   = false;
-        $matched  = "";
-        $response = array();
-        while(!feof($file)) {
-            $line = fgets($file);
-            $arr  = explode("\t", $line);
-            $list = Helper::getStorage(Helper::getToken(), $line);
-            $json = json_decode($list);
+        // $file     = fopen(storage_path("app/public/tokens.txt"), "r");
+        // $isHere   = false;
+        // $matched  = "";
+        // $response = array();
+        // while(!feof($file)) {
+        //     $line = fgets($file);
+        //     $arr  = explode("\t", $line);
+        //     $list = Helper::getStorage(Helper::getToken(), $line);
+        //     $json = json_decode($list);
 
-            DB::insert('Insert into norgesenergi (storage_token, name, phone_number, email, created_date) values (?, ?, ?, ?, ?)', [$line, $json->first_name.' '.$json->last_name, $json->phone, $json->email, $json->_created]);   
-        }
+        //     DB::insert('Insert into norgesenergi (storage_token, name, phone_number, email, created_date) values (?, ?, ?, ?, ?)', [$line, $json->first_name.' '.$json->last_name, $json->phone, $json->email, $json->_created]);   
+        // }
 
-        fclose($file);
+        // fclose($file);
         // echo Helper::storageStatus(Helper::getToken(), "ezF6ebG0090NRQdV8YKgCSFBotUXfGMfYIuKK6vftKQgTHH8WBu4ZuIBd52YaNpk", "info");        
         // echo "<br>---------------<br>";
         // echo Helper::storageStatus(Helper::getToken(), "6jHeiGjv1j4qxaSGq4WBAjxpfpE9R9NcC9xCv7McEOBmLUe3DQ7RgLNAB3LdHruv", "info");
