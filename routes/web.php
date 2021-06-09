8a4a8ae917a66e9a9137b939dc853b67276be34f
ralfhbryanperez <ralfhbryanperez@yahoo.com>
2021-06-09 17:16:59 +0200

    uploading status/profile page to live server

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('/folkeregisteret', function () {
     return view('index', ['type' => "folkeregisteret"]);
 });
 Route::get('/betaling/#{number}', 'VippsController@index');
-Route::get('/betaling', 'VippsController@check');
+Route::get('/betaling', 'VippsController@checker');
 Route::get('/profile', 'ProfileController@index');
 Route::get('/logginn', 'ProfileController@logginn');
 Route::get('/logout', function () {
